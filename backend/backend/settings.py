b12f2abf59e0956bbe38f3575c667dce33a03d25
Ruslan Tishanskiy <ruslan.tishanskiy@abpsec.ru>
2026-09-19 13:30:31 +0300

    Add media volume to docker-compose and update allowed hosts in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '<your_domain>']
+ALLOWED_HOSTS = [
+    'taski.relikt.dns.navy',
+    '158.160.132.174',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
